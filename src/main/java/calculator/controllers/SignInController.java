ebedb05e6ac3afab270a0209f2a44001ee5f127f
Bartłomiej Kotarski <bkotarski1@gmail.com>
2019-05-07 00:11:44 +0200

    Connect registration panel

--- src/main/java/calculator/controllers/SignInController.java
+++ src/main/java/calculator/controllers/SignInController.java
@@ -3,15 +3,21 @@ package calculator.controllers;
 import calculator.database.dao.UserDao;
 import calculator.exceptions.CalculatorException;
 import calculator.user.User;
+import calculator.utilies.Path;
+import calculator.utilies.ProjectLoader;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import java.sql.SQLException;
 
@@ -59,14 +65,9 @@ public class SignInController {
         }
     }
 
-    @FXML
-    void registerNewUser() {
 
-
+    public void registerNewUser(MouseEvent mouseEvent) {
+        Stage window = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+        window.setScene(new Scene(ProjectLoader.ParentLoader(Path.PATH_REGISTRATION)));
     }
-
-
-
-
-
 }
--- src/main/java/calculator/utilies/Path.java
+++ src/main/java/calculator/utilies/Path.java
@@ -3,4 +3,5 @@ package calculator.utilies;
 public class Path {
     public static final String PATH_MAIN_WINDOW = "/fxml/MainWindow.fxml";
     public static final String PATH_LOGIN = "/fxml/login/SignIn.fxml";
+    public static final String PATH_REGISTRATION = "/fxml/login/Registration.fxml";
 }
