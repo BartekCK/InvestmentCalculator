aeeea2a37c140608a68e6e29cb9a2506b87452d6
Bartłomiej Kotarski <bkotarski1@gmail.com>
2019-06-11 20:19:41 +0200

    Add transient

--- src/main/java/pl/calculator/logs/Log.java
+++ src/main/java/pl/calculator/logs/Log.java
@@ -4,6 +4,7 @@ import pl.calculator.controllers.MainWindowController;
 import pl.calculator.models.model.User;
 import java.io.Serializable;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -12,10 +13,12 @@ public class Log implements Serializable, ThisIsLogInformation {
     private LocalDateTime localDateTime;
     private String message;
     private User user;
+    private transient LocalDate localDate;
 
     public Log(String message) {
         this.localDateTime = LocalDateTime.now();
         this.message = message;
+        this.localDate = LocalDate.now();
         this.user = MainWindowController.getUser();
         if(this.user == null)
             this.user = new User("Default");
@@ -24,6 +27,6 @@ public class Log implements Serializable, ThisIsLogInformation {
 
     @Override
     public String toString() {
-        return "Log " + localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))+"> "+message+", dokonał użytkownik: "+ user+"\n";
+        return "Log " + localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))+"> "+message+", dokonał użytkownik: "+ user+"\n\t"+" Próba transient wynik == "+localDate+"\n";
     }
 }
